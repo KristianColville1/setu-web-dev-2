d30cc03c56e2ea2a4da7f8cf1dcc15b5cd5f4762
KristianColville1 <kristiancolville1@gmail.com>
2025-07-20 10:15:19 +0100

    Refactored station logic to station controller and added auth

--- routes.js
+++ routes.js
@@ -3,13 +3,20 @@ import { dashboardController } from "./src/controllers/dashboard-controller.js";
 import { aboutController } from "./src/controllers/about-controller.js";
 import { stationController } from "./src/controllers/station-controller.js";
 import {reportController} from "./src/controllers/report-controller.js";
+import { accountsController } from "./src/controllers/accounts-controller.js";
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
-router.get("/dashboard", dashboardController.dashboard);
-router.post("/dashboard/addstation", dashboardController.addStation);
-router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
+router.get("/dashboard", dashboardController.index);
+router.post("/dashboard/addstation", stationController.addStation);
+router.get("/dashboard/deletestation/:id", stationController.deleteStation);
 router.get("/about", aboutController.index);
 
 
--- src/controllers/station-controller.js
+++ src/controllers/station-controller.js
@@ -9,4 +9,19 @@ export const stationController = {
     };
     response.render("station-view", viewData);
   },
+  async addStation(request, response) {
+    const newStation = {
+      name: request.body.name,
+    };
+    console.log(`adding station ${newStation.name}`);
+    await stationStore.addStation(newStation);
+    response.redirect("/dashboard");
+  },
+
+  async deleteStation(request, response) {
+    const stationID = request.params.id;
+    console.log(`Deleting Station ${stationID}`);
+    await stationStore.deleteStationByID(stationID);
+    response.redirect("/dashboard");
+  },
 };

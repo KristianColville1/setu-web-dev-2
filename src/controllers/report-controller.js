1cbb6fd6daf98c525189da366f38eb97537e35c2
KristianColville1 <kristiancolville1@gmail.com>
2025-08-04 13:14:09 +0100

    Fix default values coming in for reports

--- src/controllers/report-controller.js
+++ src/controllers/report-controller.js
@@ -3,12 +3,16 @@ import { reportStore } from "../models/report-store.js";
 export const reportController = {
   async addReport(request, response) {
     const stationID = request.params.id;
+    // Helper function to check for empty string or undefined/null
+    const getValueOrDefault = (value, defaultValue) =>
+      value === undefined || value === null || value === "" ? defaultValue : value;
+
     const newReport = {
-      code: request.body.code,
-      temp: request.body.temp,
-      windSpeed: request.body.windSpeed,
-      windDirection: request.body.windDirection,
-      pressure: request.body.pressure,
+      code: getValueOrDefault(request.body.code, 200),
+      temp: getValueOrDefault(request.body.temp, 0.0),
+      windSpeed: getValueOrDefault(request.body.windSpeed, 0.0),
+      windDirection: getValueOrDefault(request.body.windDirection, 0),
+      pressure: getValueOrDefault(request.body.pressure, 0.0),
     };
     console.log(`Adding report for station ${stationID}`);
     await reportStore.addReport(stationID, newReport);

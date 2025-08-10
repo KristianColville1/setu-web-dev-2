5e33d252a59b1be72dbb8e3c41c21f8be0f933fd
KristianColville1 <kristiancolville1@gmail.com>
2025-08-10 09:40:17 +0100

    Fixed reports layout and password field in my account

--- src/models/station-store.js
+++ src/models/station-store.js
@@ -9,18 +9,7 @@ export const stationStore = {
     await db.read();
     const stations = db.data.stations || [];
     for (const station of stations) {
-      station.code = await reportStore.getCodeByStationId(station._id);
-      station.windDirection = await reportStore.getWindDirectionByStationId(station._id);
-      station.temp = await reportStore.getTempByStationId(station._id);
-      station.pressure = await reportStore.getPressureByStationId(station._id);
-      station.wind = await reportStore.getWindByStationId(station._id);
-      station.reports = await reportStore.getReportsByStationId(station._id);
-      station.minTemp = await reportStore.getMinTempByStationId(station._id);
-      station.maxTemp = await reportStore.getMaxTempByStationId(station._id);
-      station.minWind = await reportStore.getMinWindByStationId(station._id);
-      station.maxWind = await reportStore.getMaxWindByStationId(station._id);
-      station.minPressure = await reportStore.getMinPressureByStationId(station._id);
-      station.maxPressure = await reportStore.getMaxPressureByStationId(station._id);
+      await stationStore.getStationDetails(station);
     }
     return stations;
   },
@@ -37,6 +26,8 @@ export const stationStore = {
     await db.read();
     const station = db.data.stations.find((station) => station._id === id);
     station.reports = await reportStore.getReportsByStationId(id);
+    await stationStore.getStationDetails(station);
+    console.log(station);
     for (const report of station.reports) {
       report.name = station.name;
       report.latitude = station.latitude;
@@ -50,6 +41,12 @@ export const stationStore = {
     const stations = db.data.stations.filter((station) => station.userId === userId);
     stations.sort((a, b) => a.name.localeCompare(b.name)); // put in alphabetical order by name
     for (const station of stations) {
+      await stationStore.getStationDetails(station);
+    }
+    return stations;
+  },
+
+  async getStationDetails(station){
       station.code = await reportStore.getCodeByStationId(station._id);
       station.windDirection = await reportStore.getWindDirectionByStationId(station._id);
       station.temp = await reportStore.getTempByStationId(station._id);
@@ -62,8 +59,6 @@ export const stationStore = {
       station.maxWindSpeed = await reportStore.getMaxWindSpeedByStationId(station._id);
       station.minPressure = await reportStore.getMinPressureByStationId(station._id);
       station.maxPressure = await reportStore.getMaxPressureByStationId(station._id);
-    }
-    return stations;
   },
 
   async deleteStationByID(id) {
